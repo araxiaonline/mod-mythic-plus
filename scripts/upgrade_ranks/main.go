15c4e229679053cbccacb2ec819c1c6552262982
Ben Carter <ben.carter.dev@gmail.com>
2025-02-04 00:23:36 -0500

    updated for resist information

--- scripts/upgrade_ranks/main.go
+++ scripts/upgrade_ranks/main.go
@@ -98,7 +98,78 @@ func main() {
 			)
 
 			// Add a comma for all but the last line
-			if !(statID == STAT_SPIRIT && rank == 50) {
+			sql += ","
+			fmt.Fprintln(outputFile, sql)
+
+		}
+	}
+
+	resists := map[int]string{
+		RESIST_FROST:  "Frost",
+		RESIST_FIRE:   "Fire",
+		RESIST_NATURE: "Nature",
+		RESIST_SHADOW: "Shadow",
+		RESIST_ARCANE: "Arcane",
+	}
+
+	// Iterate over stats
+	for resistId := range resists {
+
+		resistIdbump := resistId + 5
+		for rank := 1; rank <= 50; rank++ {
+			// Material cost increases by 50 per rank
+			materialCost := 50
+			if rank < 11 {
+				materialCost = 75 + (rank-1)*35
+			}
+			if rank >= 11 && rank < 30 {
+				materialCost = 350 + (rank-11)*25
+			}
+			if rank >= 30 {
+				materialCost = 700 + (rank-30)*18
+			}
+
+			// Stat growth
+			minIncrease1 := 1 + (rank-1)/5*2
+			maxIncrease1 := 5 + (rank-1)/5*2
+
+			minIncrease2 := (minIncrease1 + maxIncrease1) / 2
+			maxIncrease3Bonus := (rank-1)/5*2 + 1
+
+			maxIncrease3 := maxIncrease1 + maxIncrease3Bonus
+			minIncrease3 := maxIncrease3 - 2
+
+			// Dice costs
+			chanceCost1 := 20 + (rank-1)*3
+			chanceCost2 := 50 + (rank-1)*3
+			chanceCost3 := 75 + (rank-1)*3
+
+			// use material ids from the mp_material_types table material1 should be common stuff.
+			materialId1 := resistIdbump*2 + 1
+
+			// material2 should be rare stuff only required after rank 10 at growth rate of 5 per rank
+			materialId2, materialCost2 := 0, 0
+			if rank > 10 {
+				materialId2 = resistIdbump*2 + 2
+				materialCost2 = (rank - 11) * 10
+			}
+
+			materialId3, materialCost3 := 0, 0
+			if rank >= 30 {
+				materialId3 = 20 // Group lot of raid only items
+				materialCost3 = (rank - 29) * 3
+			}
+
+			// Write SQL insert statement for this rank
+			sql := fmt.Sprintf(
+				"(%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d)",
+				rank, resistIdbump, materialId1, materialCost, materialId2, materialCost2, materialId3, materialCost3,
+				minIncrease1, maxIncrease1, minIncrease2, maxIncrease1, minIncrease3, maxIncrease3,
+				chanceCost1, chanceCost2, chanceCost3,
+			)
+
+			// Add a comma for all but the last line
+			if !(resistId == RESIST_ARCANE && rank == 50) {
 				sql += ","
 			}
 			fmt.Fprintln(outputFile, sql)
